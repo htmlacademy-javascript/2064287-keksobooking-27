b27ff9a2d7723953ca14bd5f0c830a9a97fd2c92
Yegor Kozlov <101704236+yamara28@users.noreply.github.com>
2022-10-03 20:06:56 +0300

    Fixing renarks after checking

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-function getRandomIntegerNumber(MIN, MAX) {
-  if (MIN < 0 || MAX < 0) { return NaN; }
-  if (MIN < MAX) {
-    MIN = Math.ceil(MIN);
-    MAX = Math.floor(MAX);
-    return `целое число из диапазона "от ${MIN} до ${MAX}": ${Math.floor(Math.random() * (MAX - MIN + 1) + MIN)}`;
+function getRandomFloatingPointNumber(min, max, amountCharactersAfterPoint) {
+  if (min < 0 || max < 0 || amountCharactersAfterPoint < 0) {
+    return NaN;
   }
-  return 'Поменяйте границы диапазона!';
-}
-getRandomIntegerNumber();
 
-
-function getRandomFloatingPointNumber(MIN, MAX, AMOUNT_CHARACTERS_AFTER_POINT) {
-  if (MIN < 0 || MAX < 0 || AMOUNT_CHARACTERS_AFTER_POINT < 0) { return NaN; }
-
-  if (MIN < MAX) {
-    return `число с плавающей точкой из диапазона "от ${MIN} до ${MAX}": ${+(Math.random() * (MAX - MIN) + MIN).toFixed(AMOUNT_CHARACTERS_AFTER_POINT)} с указанным количеством знаков после запятой ${AMOUNT_CHARACTERS_AFTER_POINT}`;
+  if (min < max) {
+    return +(Math.random() * (max - min) + min).toFixed(amountCharactersAfterPoint);
   }
-  return 'Поменяйте границы диапазона!';
+  return getRandomFloatingPointNumber(max, min, amountCharactersAfterPoint);
 }
 
 getRandomFloatingPointNumber();
 
+function getRandomintegerNumber(min, max) {
+  return getRandomFloatingPointNumber(min, max, 0);
+}
+
+getRandomintegerNumber();
