1d2a8b63772f4eb5fb684f26e39939e1106e3fca
Yegor Kozlov <101704236+yamara28@users.noreply.github.com>
2022-10-26 15:20:24 +0300

    adding validation on price with dependence os types living

--- js/formValidation.js
+++ js/formValidation.js
@@ -3,6 +3,7 @@ const TITLE = FORM.querySelector('#title');
 const PRICE = FORM.querySelector('#price');
 const ROOMS = FORM.querySelector('#room_number');
 const CAPACITY = FORM.querySelector('#capacity');
+const TYPE_OF_LIVING = FORM.querySelector('#type');
 
 const pristineConfig = {
   classTo: 'ad-form__element',
@@ -10,6 +11,20 @@ const pristineConfig = {
   errorTextParent: 'ad-form__element',
   errorTextClass: 'text-help'
 };
+const PriceForLiving = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
+const RoomsAndCapacity = {
+  1: '«для 1 гостя»',
+  2 : '«для 2 гостей» или «для 1 гостя»',
+  3 : '«для 3 гостей», «для 2 гостей» или «для 1 гостя»',
+  100 : '«не для гостей»'
+};
+
 const pristine = new Pristine(FORM, pristineConfig);
 
 const titleValidation = (value) => value.length >= 30 && value.length <= 100;
@@ -22,21 +37,36 @@ const getTitleMessage = (value) => {
 pristine.addValidator(TITLE, titleValidation, getTitleMessage, 100, true);
 
 
-const priceValidation = (value) => +value && +value <= 100000;
+TYPE_OF_LIVING.addEventListener('change', () => {
+  const selectedValue = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+  PRICE.setAttribute('min', `${PriceForLiving[selectedValue]}`);
+  PRICE.placeholder = PriceForLiving[selectedValue];
+  pristine.validate(PRICE);
+});
+
+const priceValidation = (value) => {
+  if (!value) {
+    return false;}
+  const selectedValueInTypeOfLiving = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+  const minimalValue = PriceForLiving[selectedValueInTypeOfLiving];
+
+  return +value >= minimalValue && +value <= 100000;
+};
+
 const getPriceMessage = (value) => {
-  if (!+value) {
+  const typeOfLivingSelectedValue = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+
+  if (!value) {
     return 'Поле обязательно для заполнения';
+  }
+  if (+value <= PriceForLiving[typeOfLivingSelectedValue]) {
+    return `Минимальная стоимость: ${PriceForLiving[typeOfLivingSelectedValue]}`;
   }
   return 'Максимальная стоимость: 100 000';
 };
+
 pristine.addValidator(PRICE, priceValidation, getPriceMessage, 100, true);
 
-const RoomsAndCapacity = {
-  1: '«для 1 гостя»',
-  2 : '«для 2 гостей» или «для 1 гостя»',
-  3 : '«для 3 гостей», «для 2 гостей» или «для 1 гостя»',
-  100 : '«не для гостей»'
-};
 
 const roomsValidation = () => {
   const roomValue = parseInt(ROOMS.value, 10);
@@ -51,13 +81,10 @@ const roomsValidation = () => {
   const validCapacityValuesForRoom = roomsValidCapacitiesMap[roomValue];
   return validCapacityValuesForRoom.includes(capacityValue);
 };
-
 CAPACITY.addEventListener('change', () => {
   pristine.validate(ROOMS);
 });
-
 const getRoomsMessage = () => RoomsAndCapacity[ROOMS.value];
-
 pristine.addValidator(ROOMS, roomsValidation, getRoomsMessage, 100, true);
 
 FORM.addEventListener('submit', (evt) => {
