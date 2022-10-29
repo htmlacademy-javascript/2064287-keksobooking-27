e8ff015ea20fcbf0d9855b3ddf4f41a749a29dbf
Keks <keks@htmlacademy.ru>
2022-10-29 14:09:18 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/data.js
@@ -0,0 +1,96 @@
+import {getRandomArrayElement, getRandomIntegerNumber, getShuffledArrayWithRandomLength, getRandomFloatingPointNumber} from './util.js';
+
+const RANDOM_ADS_AMOUNT = 10;
+const MIN_PRICE = 0;
+const MAX_PRICE = 100000;
+const MIN_ROOMS_AMOUNT = 1;
+const MAX_ROOMS_AMOUNT = 100;
+const MIN_GUESTS_AMOUNT = 1;
+const MAX_GUESTS_AMOUNT = 3;
+const MIN_LAT = 35.65000;
+const MAX_LAT = 35.70000;
+const MIN_LNG = 139.70000;
+const MAX_LNG = 139.80000;
+const AMOUNT_SYNBOLS_AFTER_POINT_IN_LOCATION = 5;
+const TITLES = [
+  'Chateau',
+  'Palace',
+  'Wigwam',
+  'Flat',
+  'House',
+  'Semi-detached house',
+  'Duplex',
+  'Loft',
+  'Tyneside flat',
+  'Triple-decker house'
+];
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUT = CHECKIN;
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTIONS = [
+  'Not bad',
+  'It could be bettet',
+  'Awesome',
+  'Cool',
+  'Greate',
+  'Much better',
+  'So-so'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const getRandomAd = (_, i) => {
+  const location = {
+    lat: getRandomFloatingPointNumber(MIN_LAT, MAX_LAT, AMOUNT_SYNBOLS_AFTER_POINT_IN_LOCATION),
+    lng: getRandomFloatingPointNumber(MIN_LNG, MAX_LNG, AMOUNT_SYNBOLS_AFTER_POINT_IN_LOCATION),
+  };
+
+  return {
+    avatar: `img/avatars/user${String(i + 1).padStart(2, '0')}.png`,
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomIntegerNumber(MIN_PRICE, MAX_PRICE),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomIntegerNumber(MIN_ROOMS_AMOUNT, MAX_ROOMS_AMOUNT),
+      guests: getRandomIntegerNumber(MIN_GUESTS_AMOUNT, MAX_GUESTS_AMOUNT),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: getShuffledArrayWithRandomLength(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getShuffledArrayWithRandomLength(PHOTOS),
+    },
+    location
+  };
+};
+
+const generatedRandomAD = (count = RANDOM_ADS_AMOUNT) => Array.from({length : count}, getRandomAd);
+
+export {generatedRandomAD};
---
+++ 9/js/form.js
@@ -0,0 +1,134 @@
+const FORM = document.querySelector('.ad-form');
+const TITLE = FORM.querySelector('#title');
+const ADDRESS = FORM.querySelector('#address');
+const PRICE = FORM.querySelector('#price');
+const ROOMS = FORM.querySelector('#room_number');
+const CAPACITY = FORM.querySelector('#capacity');
+const TYPE_OF_LIVING = FORM.querySelector('#type');
+const CHECKIN = FORM.querySelector('#timein');
+const CHECKOUT = FORM.querySelector('#timeout');
+const SLIDER = FORM.querySelector('.ad-form__slider');
+
+const pristineConfig = {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'text-help'
+};
+const PriceForLiving = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
+const RoomsAndCapacity = {
+  1: '«для 1 гостя»',
+  2: '«для 2 гостей» или «для 1 гостя»',
+  3: '«для 3 гостей», «для 2 гостей» или «для 1 гостя»',
+  100: '«не для гостей»'
+};
+
+const pristine = new Pristine(FORM, pristineConfig);
+
+const titleValidation = (value) => value.length >= 30 && value.length <= 100;
+const getTitleMessage = (value) => {
+  if (!value.length) {
+    return 'Поле обязательно для заполнения';
+  }
+  return `Необходимо ввести от 30 до 100 символов. Вы ввели: ${value.length}`;
+};
+pristine.addValidator(TITLE, titleValidation, getTitleMessage, 100, true);
+
+TYPE_OF_LIVING.addEventListener('change', () => {
+  const selectedValue = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+  PRICE.setAttribute('min', `${PriceForLiving[selectedValue]}`);
+  PRICE.placeholder = PriceForLiving[selectedValue];
+  pristine.validate(PRICE);
+});
+
+const priceValidation = (value) => {
+  if (!value) {
+    return false;
+  }
+  const selectedValueInTypeOfLiving = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+  const minimalValue = PriceForLiving[selectedValueInTypeOfLiving];
+
+  return +value >= minimalValue && +value <= 100000;
+};
+
+const getPriceMessage = (value) => {
+  const typeOfLivingSelectedValue = TYPE_OF_LIVING.options[TYPE_OF_LIVING.selectedIndex].value;
+
+  if (!value) {
+    return 'Поле обязательно для заполнения';
+  }
+  if (+value <= PriceForLiving[typeOfLivingSelectedValue]) {
+    return `Минимальная стоимость: ${PriceForLiving[typeOfLivingSelectedValue]}`;
+  }
+  return 'Максимальная стоимость: 100 000';
+};
+
+pristine.addValidator(PRICE, priceValidation, getPriceMessage, 100, true);
+
+
+const roomsValidation = () => {
+  const roomValue = parseInt(ROOMS.value, 10);
+  const capacityValue = parseInt(CAPACITY.value, 10);
+  const roomsValidCapacitiesMap = {
+    1: [1],
+    2: [1, 2],
+    3: [1, 2, 3],
+    100: [0],
+  };
+
+  const validCapacityValuesForRoom = roomsValidCapacitiesMap[roomValue];
+  return validCapacityValuesForRoom.includes(capacityValue);
+};
+CAPACITY.addEventListener('change', () => {
+  pristine.validate(ROOMS);
+});
+const getRoomsMessage = () => RoomsAndCapacity[ROOMS.value];
+pristine.addValidator(ROOMS, roomsValidation, getRoomsMessage, 100, true);
+
+
+CHECKIN.addEventListener('change', () => {
+  const selectedValueCheckin = CHECKIN.options[CHECKIN.selectedIndex].value;
+  CHECKOUT.value = selectedValueCheckin;
+});
+CHECKOUT.addEventListener('change', () => {
+  const selectedValueCheckout = CHECKOUT.options[CHECKOUT.selectedIndex].value;
+  CHECKIN.value = selectedValueCheckout;
+});
+
+
+FORM.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    FORM.submit();
+  }
+});
+
+
+noUiSlider.create(SLIDER, {
+  range: {
+    min: 0,
+    max: 100000,
+  },
+  start: 0,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: (value) => value.toFixed(),
+    from: (value) => +value
+  }
+});
+
+SLIDER.noUiSlider.on('update', () => {
+  PRICE.value = SLIDER.noUiSlider.get();
+});
+PRICE.addEventListener('change', () => {
+  SLIDER.noUiSlider.set(PRICE.value);
+});
+
+export { ADDRESS };
