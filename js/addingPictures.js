426392dbfe0afd2d298e22bf7a737fffa45ee6d0
Yegor Kozlov <101704236+yamara28@users.noreply.github.com>
2022-11-14 11:27:31 +0300

    refactoring of function

--- js/addingPictures.js
+++ js/addingPictures.js
@@ -1,25 +1,26 @@
+import { isFileTypeMatches } from './util.js';
+
 const AVATAR_INPUT = document.querySelector('.ad-form__field input[type=file]');
 const AVATAR_PREVIEW = document.querySelector('.ad-form-header__preview img');
 const PHOTO_INPUT = document.querySelector('.ad-form__upload input[type=file]');
 const PHOTO_PREVIEW = document.querySelector('.ad-form__photo');
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
+
 AVATAR_INPUT.addEventListener('change', () => {
   const file = AVATAR_INPUT.files[0];
-  const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  isFileTypeMatches(file, FILE_TYPES);
 
-  if (matches) {
+  if (isFileTypeMatches) {
     AVATAR_PREVIEW.src = URL.createObjectURL(file);
   }
 });
 
 PHOTO_INPUT.addEventListener('change', () => {
   const file = PHOTO_INPUT.files[0];
-  const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  isFileTypeMatches(file, FILE_TYPES);
 
-  if (matches) {
+  if (isFileTypeMatches) {
     const photoPreviewTag = document.createElement('img');
     photoPreviewTag.src = URL.createObjectURL(file);
     photoPreviewTag.style.width = '100%';
